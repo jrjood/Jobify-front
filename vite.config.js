3c5c21554aa02a2088a2c66978af1c822a75f68a
jrjood <youssifbebo@gmail.com>
2025-04-08 02:18:23 +0200

    Fix for deploying homepage

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Jobify-front/', // Set repo name here
+  // base: '/Jobify-front/', // Set repo name here
   build: {
     outDir: 'dist',
   },
